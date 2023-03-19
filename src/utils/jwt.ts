a28f68a14753fca0d35b1b47d3d87e4351fbf087
satriyoaji <satriyoaji27@gmail.com>
2023-03-19 18:13:51 +0700

    Change to dynamic private & public key

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,19 +1,23 @@
-import jwt, { SignOptions } from 'jsonwebtoken';
+import jwt, {SignOptions} from 'jsonwebtoken';
 import config from 'config';
 import * as fs from "fs";
-const privateKEY  = fs.readFileSync('./config/private.key', 'utf8');
-const publicKEY  = fs.readFileSync('./config/public.key', 'utf8');
 
 export const signJwt = (
   payload: Object,
   keyName: 'accessTokenPrivateKey' | 'refreshTokenPrivateKey',
   options: SignOptions
 ) => {
-  const privateKey = Buffer.from(
-    config.get<string>(keyName),
-    'base64'
-  ).toString('ascii');
-  return jwt.sign(payload, privateKEY, {
+  // const privateKey = Buffer.from(
+  //   config.get<string>(keyName),
+  //   'base64'
+  // ).toString('ascii');
+  const privateKey = config.get<string>(keyName)
+
+  let privateKeyLoaded = (privateKey === "./config/private.key" || privateKey === "") ?
+      fs.readFileSync(privateKey, 'utf8') :
+      privateKey;
+
+  return jwt.sign(payload, privateKeyLoaded, {
     ...(options && options),
     algorithm: 'RS512',
   });
@@ -24,13 +28,17 @@ export const verifyJwt = <T>(
   keyName: 'accessTokenPublicKey' | 'refreshTokenPublicKey'
 ): T | null => {
   try {
-    const publicKey = Buffer.from(
-      config.get<string>(keyName),
-      'base64'
-    ).toString('ascii');
-    const decoded = jwt.verify(token, publicKEY) as T;
+    // const publicKey = Buffer.from(
+    //   config.get<string>(keyName),
+    //   'base64'
+    // ).toString('ascii');
+    const publicKey = config.get<string>(keyName)
 
-    return decoded;
+    let publicKeyLoaded = (publicKey === "./config/private.key" || publicKey === "") ?
+        fs.readFileSync(publicKey, 'utf8') :
+        publicKey;
+
+    return jwt.verify(token, publicKeyLoaded) as T;
   } catch (error) {
     return null;
   }
